0b6d56cdc5e19a7f053773500ba9b1a53ac24b00
Esca-MMC <esca.mmc@gmail.com>
2019-11-15 07:09:09 -0600

    Refactor SMAPI event methods

---
+++ FarmTypeManager/Events/AddSMAPIEvents.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+namespace FarmTypeManager
+{
+    public partial class ModEntry : Mod
+    {
+        private void AddSMAPIEvents(IModHelper helper)
+        {
+            //tell SMAPI to run event methods when necessary
+            helper.Events.GameLoop.DayStarted += DayStarted;
+            helper.Events.GameLoop.TimeChanged += TimeChanged;
+            helper.Events.GameLoop.DayEnding += DayEnding;
+            helper.Events.GameLoop.ReturnedToTitle += ReturnedToTitle;
+        }
+    }
+}
