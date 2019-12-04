df77d8d37ce3e96f480244804f0cc0415f797e3f
Esca-MMC <esca.mmc@gmail.com>
2019-12-03 21:41:50 -0600

    Implement Size property for saved objects

--- FarmTypeManager/Classes/SavedObject.cs
+++ FarmTypeManager/Classes/SavedObject.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using StardewModdingAPI;
+using StardewValley.Monsters;
 
 namespace FarmTypeManager
 {
@@ -17,6 +19,55 @@ namespace FarmTypeManager
             public int? DaysUntilExpire { get; set; }
             public MonsterType MonType { get; set; }
             public StardewTime SpawnTime { get; set; } = 600; //default to 6:00AM for backward compatibility
+
+            /// <summary>A point representing this object's size in tiles.</summary>
+            public Point Size
+            {
+                get
+                {
+                    switch (Type)
+                    {
+                        case ObjectType.Forage:
+                        case ObjectType.Ore:
+                            return new Point(1, 1);
+                        case ObjectType.LargeObject:
+                            if (ID == 190 || ID == 254 || ID == 276) //if this seems to be a giant crop
+                                return new Point(3, 3);
+                            else //if this seems to be a resource clump
+                                return new Point(2, 2);
+                        case ObjectType.Monster:
+                            if (!MonsterSizeCache.ContainsKey(MonType.MonsterName)) //if this monster type's size has not been cached yet
+                            {
+                                if (MonType.MonsterName.Contains(".")) //if this is an external monster class
+                                {
+                                    Type externalType = Utility.GetTypeFromName(MonType.MonsterName, typeof(Monster)); //find a monster subclass with a matching name
+                                    Monster monster = (Monster)Activator.CreateInstance(externalType, Vector2.Zero); //create a monster with the Vector2 constructor (the tile should be irrelevant)
+                                    int width = Convert.ToInt32(Math.Ceiling(((double)monster.Sprite.SpriteWidth) / 16)); //get the monster's sprite width in tiles, rounded up
+                                    if (width <= 0)
+                                        width = 1; //enforce minimum 1x1 size
+                                    MonsterSizeCache.Add(MonType.MonsterName, new Point(width, width)); //use the width for both dimensions (to avoid problems with "tall" 1x1 monster sprites)
+                                }
+                                else if //if this is a known type of 2x2 monster
+                                (
+                                    MonType.MonsterName.StartsWith("big", StringComparison.OrdinalIgnoreCase) ||
+                                    MonType.MonsterName.StartsWith("serpent", StringComparison.OrdinalIgnoreCase) ||
+                                    MonType.MonsterName.StartsWith("rex", StringComparison.OrdinalIgnoreCase) ||
+                                    MonType.MonsterName.StartsWith("pepper", StringComparison.OrdinalIgnoreCase) ||
+                                    MonType.MonsterName.StartsWith("dino", StringComparison.OrdinalIgnoreCase)
+                                )
+                                    MonsterSizeCache.Add(MonType.MonsterName, new Point(2, 2));
+                                else //if this is a known type of 1x1 monster
+                                    MonsterSizeCache.Add(MonType.MonsterName, new Point(1, 1));
+                            }
+
+                            return MonsterSizeCache[MonType.MonsterName];
+                        default:
+                            return new Point(1, 1); //use default size for unknown enum values
+                    }
+                }
+            }
+
+            private static Dictionary<string, Point> MonsterSizeCache = new Dictionary<string, Point>();
 
             /// <param name="mapName">The name of the in-game location where this object exists.</param>
             /// <param name="tile">A tile indicating where this object exists.</param>
