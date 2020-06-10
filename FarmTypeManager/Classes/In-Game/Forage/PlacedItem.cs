0fa3becb9d17a84bdcf6400640ceadb73b2502c5
Esca-MMC <esca.mmc@gmail.com>
2020-06-09 19:41:23 -0500

    Add foraging-related behavior to PlacedItem

--- FarmTypeManager/Classes/In-Game/Forage/PlacedItem.cs
+++ FarmTypeManager/Classes/In-Game/Forage/PlacedItem.cs
@@ -74,6 +74,7 @@ namespace FarmTypeManager
                 }
             }
 
+            /// <summary>Determines whether a character is obstructed by this placed item when moving through it.</summary>
             public override bool isPassable(Character c = null)
             {
                 if (Constants.TargetPlatform == GamePlatform.Android) //if this is SDV Android
@@ -82,6 +83,7 @@ namespace FarmTypeManager
                     return base.isPassable(c);
             }
 
+            /// <summary>Performs behavior when a character collides with this placed item.</summary>
             public override void doCollisionAction(Rectangle positionOfCollider, int speedOfCollision, Vector2 tileLocation, Character who, GameLocation location)
             {
                 if (Constants.TargetPlatform == GamePlatform.Android) //if this is running on Android
@@ -93,6 +95,9 @@ namespace FarmTypeManager
                 }
             }
 
+            /// <summary>Performs behavior when a player "grabs" this placed item.</summary>
+            /// <remarks>This method does not seem to be properly invoked by Android.
+            /// <see cref="isPassable(Character)"/>and <see cref="doCollisionAction(Rectangle, int, Vector2, Character, GameLocation)"/> are used as a conditional workaround.</remarks>
             public override bool performUseAction(Vector2 tileLocation, GameLocation location)
             {
                 if (!Game1.player.canMove || this.isTemporarilyInvisible || !Game1.player.couldInventoryAcceptThisItem(Item)) //if this isn't the local player OR they can't currently pick this up
@@ -100,8 +105,11 @@ namespace FarmTypeManager
 
                 //add the contained item to the player's inventory and remove this placed item
 
+                SetForageQuality(location); //if this is forage, set its quality
                 if (Game1.player.addItemToInventoryBool(Item, true)) //add this item to the player's inventory; if successful,
                 {
+                    OnForagePickup(Item, location); //if this is forage, perform related tasks
+
                     location.localSound("pickUpItem");
                     DelayedAction.playSoundAfterDelay("coin", 300, location, -1);
                     Game1.player.animateOnce(279 + Game1.player.FacingDirection); //do the player's "pick up object" animation
@@ -126,6 +134,66 @@ namespace FarmTypeManager
                 }  
 
                 return true; //this placed item was used
+            }
+
+            /// <summary>Assigns quality to this item if it is a forage object based on the local player's Foraging skill and professions.</summary>
+            /// <param name="location">The current location of this item.</param>
+            private void SetForageQuality(GameLocation location)
+            {
+                if (Item is StardewValley.Object obj && obj.isForage(location)) //if this item is forage
+                {
+                    //determine forage quality
+                    if (Game1.player.professions.Contains(Farmer.botanist)) //if the player has the Botanist profession
+                        obj.Quality = StardewValley.Object.bestQuality; //maximize the object's quality
+                    else
+                    {
+                        //imitate Stardew's random seed, which produces the same result when give the same object position and in-game day
+                        Random random = new Random((int)Game1.uniqueIDForThisGame / 2 + (int)Game1.stats.DaysPlayed + (int)currentTileLocation.X + (int)currentTileLocation.Y * 777);
+                        
+                        //set random qualities based on the player's Foraging skill
+                        if (random.NextDouble() < Game1.player.ForagingLevel / 30.0)
+                            obj.Quality = StardewValley.Object.highQuality; //gold quality
+                        else if (random.NextDouble() < Game1.player.ForagingLevel / 15.0)
+                            obj.Quality = StardewValley.Object.medQuality; //silver quality
+                        else
+                            obj.Quality = StardewValley.Object.lowQuality; //bronze quality, a.k.a. normal/no quality
+                    }
+                }
+            }
+
+            /// <summary>Performs post-pickup forage object behaviors. These include Gatherer profession effects, experience gain, and statistics updates.</summary>
+            /// <param name="item">The possible forage item.</param>
+            /// <param name="location">The item's current location.</param>
+            private void OnForagePickup(Item item, GameLocation location)
+            {
+                if (Item is StardewValley.Object obj && obj.isForage(location)) //if this is a forage object
+                {
+                    int totalStackSize = obj.Stack;
+
+                    //check the "double forage" profession chance
+                    if (Game1.player.professions.Contains(Farmer.gatherer) && Utility.RNG.NextDouble() < 0.2) //20% chance if the player has the Gatherer profession
+                    {
+                        totalStackSize *= 2; //double the recorded total stack size
+
+                        Item secondStack = Item.getOne(); //copy the item
+                        secondStack.Stack = Item.Stack; //copy the original stack size
+
+                        if (!Game1.player.addItemToInventoryBool(secondStack, true)) //add the second stack to the player's inventory; if there is not enough space for all of it,
+                        {
+                            //drop the remaining stack on the ground
+                            Point centerOfPlayer = Game1.player.GetBoundingBox().Center; //get the player's center
+                            Vector2 dropPosition = new Vector2(centerOfPlayer.X, centerOfPlayer.Y); //create a Vector2 of the player's center
+                            Game1.createItemDebris(secondStack, dropPosition, Game1.player.FacingDirection, location); //drop the item at the player's position
+                        }
+                    }
+
+                    if (location.isFarmBuildingInterior()) //if this is was collected inside a farm building
+                        Game1.player.gainExperience(Farmer.farmingSkill, 5 * totalStackSize); //gain farming experience multiplied by stack size
+                    else
+                        Game1.player.gainExperience(Farmer.foragingSkill, 7 * totalStackSize); //gain foraging experience multiplied by stack size
+
+                    Game1.stats.ItemsForaged += (uint)totalStackSize; //increase the "items foraged" stat by the object's stack size
+                }
             }
         }
     }
