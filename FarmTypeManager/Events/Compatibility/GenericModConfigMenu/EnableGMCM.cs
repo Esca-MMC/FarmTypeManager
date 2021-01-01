3011da9e6b198dd11832044b257b1505926c8e62
Esca-MMC <esca.mmc@gmail.com>
2021-01-01 12:09:52 -0600

    Add EPU Preconditions list to ExtraConditions

--- FarmTypeManager/Events/Compatibility/GenericModConfigMenu/EnableGMCM.cs
+++ FarmTypeManager/Events/Compatibility/GenericModConfigMenu/EnableGMCM.cs
@@ -20,15 +20,23 @@ namespace FarmTypeManager
             {
                 GenericModConfigMenuAPI api = Helper.ModRegistry.GetApi<GenericModConfigMenuAPI>("spacechase0.GenericModConfigMenu"); //attempt to get GMCM's API instance
 
-                if (api == null) //if the API is not available
+                if (api == null) //if the API is NOT available
+                {
+                    Monitor.Log($"Optional API not found: Generic Mod Config Menu (GMCM).", LogLevel.Trace);
                     return;
+                }
+                else //if the API is available
+                {
+                    Monitor.Log($"Optional API found: Generic Mod Config Menu (GMCM).", LogLevel.Trace);
+                }
 
-                api.RegisterModConfig(ModManifest, () => Utility.MConfig = new ModConfig(), () => Helper.WriteConfig(Utility.MConfig)); //register "revert to default" and "write" methods for this mod's config
+                api.RegisterModConfig(ModManifest, () => ResetConfigToDefault(), () => Helper.WriteConfig(Utility.MConfig)); //register "revert to default" and "write" methods for this mod's config
 
                 //register an option for each of this mod's config settings
                 api.RegisterSimpleOption(ModManifest, "Enable console commands", "Uncheck this box to disable FTM's console commands, e.g. for mod compatibility.\nNOTE: This will not take effect until Stardew Valley is restarted.", () => Utility.MConfig.EnableConsoleCommands, (bool val) => Utility.MConfig.EnableConsoleCommands = val);
                 api.RegisterSimpleOption(ModManifest, "Enable content packs", "Uncheck this box to disable all FTM content packs.\nOnly the \"personal\" files in FarmTypeManager/data will be used.", () => Utility.MConfig.EnableContentPacks, (bool val) => Utility.MConfig.EnableContentPacks = val);
                 api.RegisterSimpleOption(ModManifest, "Enable trace log messages", "Uncheck this box to disable FTM's [TRACE] message type in SMAPI's log files.\nLogs will be smaller but provide less info.", () => Utility.MConfig.EnableTraceLogMessages, (bool val) => Utility.MConfig.EnableTraceLogMessages = val);
+                api.RegisterSimpleOption(ModManifest, "Enable EPU debug messages", "Check this box to enable Expanded Preconditions Utility (EPU) debug messages.\nThis can be helpful when testing preconditions.", () => EPUDebugMessages, (bool val) => EPUDebugMessages = val);
                 api.RegisterSimpleOption(ModManifest, "Monster limit per location", "The maximum number of monsters FTM will spawn on a single map.\nEnter NULL for unlimited monsters.", () => MonsterLimitAsString, (string val) => MonsterLimitAsString = val);
             }
             catch (Exception ex)
@@ -36,6 +44,38 @@ namespace FarmTypeManager
                 Utility.Monitor.Log($"An error happened while loading FTM's GMCM options menu. That menu might be missing or fail to work. The auto-generated error message has been added to the log.", LogLevel.Warn);
                 Utility.Monitor.Log($"----------", LogLevel.Trace);
                 Utility.Monitor.Log($"{ex.ToString()}", LogLevel.Trace);
+            }
+        }
+
+        private void ResetConfigToDefault()
+        {
+            Utility.MConfig = new ModConfig(); //create and use a new config with default settings
+            EPUDebugMessages = Utility.MConfig.EnableEPUDebugMessages; //re-initialize EPU's API, if applicable
+        }
+
+        private bool EPUDebugMessages
+        {
+            get
+            {
+                return Utility.MConfig.EnableEPUDebugMessages; //return the current value
+            }
+
+            set
+            {
+                if (Utility.EPUConditionsChecker != null) //if EPU's API instance exists
+                {
+                    try
+                    {
+                        Utility.EPUConditionsChecker.Initialize(value, this.ModManifest.UniqueID); //re-initialize the conditions checker
+                        Utility.MConfig.EnableEPUDebugMessages = value; //set the new value in the config
+                    }
+                    catch (Exception ex)
+                    {
+                        Utility.Monitor.Log($"An error occurred while trying to modify EPU's API debug message setting. Please report this to FTM's developer and/or manually change the EPU setting in FTM's config.json file. The auto-generated error message has been added to your log.", LogLevel.Warn);
+                        Utility.Monitor.Log($"----------", LogLevel.Trace);
+                        Utility.Monitor.Log($"{ex.ToString()}", LogLevel.Trace);
+                    }
+                }
             }
         }
 
