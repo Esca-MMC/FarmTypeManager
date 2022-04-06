ef4b4efc074534dd6d79a263d43624adbadf7ba7
Esca-MMC <esca.mmc@gmail.com>
2022-04-06 13:05:25 -0500

    Remove obsolete fix for dust sprite bug

--- FarmTypeManager/Utility/Spawning/Monsters/SpawnMonster.cs
+++ FarmTypeManager/Utility/Spawning/Monsters/SpawnMonster.cs
@@ -276,7 +276,7 @@ namespace FarmTypeManager
                     case "spirit":
                     case "dustspirit":
                     case "dust spirit":
-                        monster = new DustSpiritFTM(tile);
+                        monster = new DustSpirit(tile);
                         break;
                     case "dwarvishsentry":
                     case "dwarvish sentry":
