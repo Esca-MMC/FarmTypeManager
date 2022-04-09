29a21ea26e1ea5dca2109295ff53fd39511f19a8
Esca-MMC <esca.mmc@gmail.com>
2022-04-09 18:07:01 -0500

    Lower DayStarted priority to avoid Content Patcher update issues

--- FarmTypeManager/Events/DayStarted.cs
+++ FarmTypeManager/Events/DayStarted.cs
@@ -1,4 +1,5 @@
 ﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
 using StardewValley;
 using System;
 
@@ -7,6 +8,7 @@ namespace FarmTypeManager
     public partial class ModEntry : Mod
     {
         /// <summary>Tasks performed after the game begins a new day, including when loading a save.</summary>
+        [EventPriority(EventPriority.Low)] //run after most other events to allow Content Patcher token updates, etc
         private void DayStarted(object sender, EventArgs e)
         {
             //attempt to load the config.json ModConfig file and update its settings
