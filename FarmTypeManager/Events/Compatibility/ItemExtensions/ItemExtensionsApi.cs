e708a6780da6d94f0723d2050863750af8b9a079
Esca-MMC <esca.mmc@gmail.com>
2024-07-18 12:18:38 -0500

    Update/trim Item Extensions API

--- FarmTypeManager/Events/Compatibility/ItemExtensions/ItemExtensionsApi.cs
+++ FarmTypeManager/Events/Compatibility/ItemExtensions/ItemExtensionsApi.cs
@@ -9,28 +9,21 @@ namespace ItemExtensions
     public interface IApi
     {
         /// <summary>
-        /// Checks for resource data in the mod.
-        /// </summary>
-        /// <param name="id">Qualified item ID</param>
-        /// <param name="health">MinutesUntilReady value</param>
-        /// <param name="itemDropped">Item dropped by ore</param>
-        /// <returns>Whether the object has ore data.</returns>
-        bool IsResource(string id, out int? health, out string itemDropped);
-
-        /// <summary>
-        /// Checks mod's menu behaviors. If a target isn't provided, it'll search whether any exist.
+        /// Checks for a qualified id in modded clump data.
         /// </summary>
         /// <param name="qualifiedItemId">Qualified item ID.</param>
-        /// <param name="target">Item to search behavior for. (Qualified item ID)</param>
-        /// <returns>Whether this item has menu behavior for target.</returns>
-        bool HasBehavior(string qualifiedItemId, string target);
-
+        /// <returns>Whether this id is a clump's.</returns>
         bool IsClump(string qualifiedItemId);
 
-        bool TrySpawnClump(string itemId, Vector2 position, string locationName, out string error, bool avoidOverlap = false);
-
+        /// <summary>
+        /// Tries to spawn a clump.
+        /// </summary>
+        /// <param name="itemId">The clump ID.</param>
+        /// <param name="position">Tile position.</param>
+        /// <param name="location">Location to use.</param>
+        /// <param name="error">Error string, if applicable.</param>
+        /// <param name="avoidOverlap">Avoid overlapping with other clumps.</param>
+        /// <returns>Whether spawning succeeded.</returns>
         bool TrySpawnClump(string itemId, Vector2 position, GameLocation location, out string error, bool avoidOverlap = false);
-
-        List<string> GetCustomSeeds(string itemId, bool includeSource, bool parseConditions = true);
     }
 }
