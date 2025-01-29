2551833e735f1508c429869af6cdb3120279a1cb
Esca-MMC <esca.mmc@gmail.com>
2025-01-29 02:21:55 -0600

    Detach ModConfig from ModEntry, tweak contents

--- FarmTypeManager/Classes/Config/ModConfig.cs
+++ FarmTypeManager/Classes/Config/ModConfig.cs
@@ -2,30 +2,25 @@
 
 namespace FarmTypeManager
 {
-    public partial class ModEntry : Mod
+    /// <summary>Represents this mod's config.json file settings.</summary>
+    public class ModConfig
     {
-        //config.json, used by all players/saves for shared functions
-        private class ModConfig
-        {
-            public bool EnableWhereAmICommand //(setting deprecated/renamed) enable the "whereami" command in the SMAPI console
-            {
-                set
-                {
-                    EnableConsoleCommands = value;
-                }
-            }
-            public bool EnableConsoleCommands { get; set; } = true; //enable this mod's SMAPI console commands
-            public bool EnableContentPacks { get; set; } = true; //enable any content packs for this mod
-            public bool EnableTraceLogMessages { get; set; } = true; //allow the mod to generate trace-level log messages (which tend to spam the "SMAPI for developers" console)
-            public bool EnableEPUDebugMessages { get; set; } = false; //allow Expanded Preconditions Utility (EPU) to generate debug-level messages about individual precondition checks
-            public int? MonsterLimitPerLocation { get; set; } = null; //an optional number of monsters allowed per game location; the mod won't spawn more monsters while this many exist
+        /// <summary>Controls whether this mod's console commands are enabled.</summary>
+        public bool EnableConsoleCommands { get; set; } = true;
+        /// <summary>Controls whether this mod's content packs are enabled.</summary>
+        public bool EnableContentPacks { get; set; } = true;
+        /// <summary>Controls whether this mod generates any trace-level messages in the console and log files.</summary>
+        public bool EnableTraceLogMessages { get; set; } = true;
+        /// <summary>Controls whether Expanded Preconditions Utility (EPU) will generate debug messages about this mod's precondition checks.</summary>
+        public bool EnableEPUDebugMessages { get; set; } = false;
+        /// <summary>Controls the maximum number of monsters this mod will spawn simultaneously at an in-game location. If this many monsters currently exist, any further spawns will be skipped. If null, unlimited monsters are allowed.</summary>
+        public int? MonsterLimitPerLocation { get; set; } = null;
 
-            public ModConfig()
+        public ModConfig()
+        {
+            if (Constants.TargetPlatform == GamePlatform.Android) //if a new config.json is created on an Android device
             {
-                if (Constants.TargetPlatform == GamePlatform.Android) //if a new config.json is created on an Android device
-                {
-                    MonsterLimitPerLocation = 50; //use the community-recommended default monster limit
-                }
+                MonsterLimitPerLocation = 50; //default to the community-recommended default monster limit (note: this info is outdated)
             }
         }
     }
