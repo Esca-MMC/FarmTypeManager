2c52ddf73489a433377e07e985d872682a94e71b
Esca-MMC <esca.mmc@gmail.com>
2025-06-27 11:20:18 -0500

    Reimplement ExtraLoot to disable Burglar's Ring

--- FarmTypeManager/Harmony Patches/ApplyHarmonyPatches.cs
+++ FarmTypeManager/Harmony Patches/ApplyHarmonyPatches.cs
@@ -14,6 +14,7 @@ namespace FarmTypeManager
             var harmony = new Harmony(this.ModManifest.UniqueID); //create this mod's Harmony instance
 
             //apply all patches
+            HarmonyPatch_DisableExtraLoot.ApplyPatch(harmony);
             HarmonyPatch_DisableFurniturePickup.ApplyPatch(harmony);
             HarmonyPatch_InstantKillImmunity.ApplyPatch(harmony);
             HarmonyPatch_OptimizeMonsterCode.ApplyPatch(harmony);
