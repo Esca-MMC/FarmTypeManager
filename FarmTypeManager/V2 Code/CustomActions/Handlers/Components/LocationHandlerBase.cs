19e8e899a77eb3f40c1f075ad62135742e442c98
Esca-MMC <esca.mmc@gmail.com>
2025-09-26 13:41:39 -0500

    Further abstract handler base

---
+++ FarmTypeManager/V2 Code/CustomActions/Handlers/Components/LocationHandlerBase.cs
@@ -0,0 +1,105 @@
+﻿using FarmTypeManager.TileQueries;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewValley;
+using StardewValley.Delegates;
+using StardewValley.Internal;
+using System;
+using System.Collections.Generic;
+
+namespace FarmTypeManager.CustomActions
+{
+    /// <summary>A base for handlers that perform actions at a customizable set of locations.</summary>
+    public abstract class LocationHandlerBase<TSettings> : ICustomActionHandler where TSettings : class, ILocationSettings, ITimesToPerformSettings
+    {
+        /************************/
+        /* ICustomActionHandler */
+        /************************/
+
+        public string ProviderModId => FTMUtility.Manifest?.UniqueID;
+        public Type SettingsType => typeof(TSettings);
+        public virtual bool TryPerform(string actionType, object rawSettings, GameStateQueryContext queryContext, TriggerActionContext triggerContext, out string error)
+        {
+            var settings = rawSettings as TSettings;
+            if (rawSettings == null)
+            {
+                error = "The provided settings are null.";
+                return false;
+            }
+            else if (settings == null)
+            {
+                error = $"The provided settings are an unsupported type: \"{rawSettings.GetType()?.ToString() ?? "null"}\".";
+                return false;
+            }
+
+            List<GameLocation> locations = settings.GetActiveLocations();
+            if (locations.Count <= 0)
+            {
+                error = null;
+                return true;
+            }
+
+            if (settings.MinTimes > settings.MaxTimes)
+            {
+                error = $"MinTimes ({settings.MinTimes}) is greater than MaxTimes ({settings.MaxTimes}).";
+                return false;
+            }
+
+            //get a random number from min to max, apply modifiers, and round to the nearest integer
+            int times = settings.GetRandomTimes(queryContext.Location, queryContext.Player, queryContext.TargetItem, queryContext.InputItem, queryContext.Random);
+            if (times <= 0)
+            {
+                error = null;
+                return true;
+            }
+
+            switch (settings.LocationListMode)
+            {
+                case ILocationSettings.LocationListModes.All:
+                    foreach (GameLocation location in locations)
+                    {
+                        if (!TryActionAtLocation(location, settings, queryContext, triggerContext, times, out error))
+                            return false;
+                    }
+                    break;
+
+                case ILocationSettings.LocationListModes.Random:
+                default:
+                    int count = locations.Count;
+                    Dictionary<int, int> timesForEachLocationIndex = new(count); //key = random index in the location list; value = number of times to use that location
+                    for (int x = 0; x < times; x++)
+                    {
+                        int index = FTMUtility.Random.Next(count);
+                        if (timesForEachLocationIndex.ContainsKey(index))
+                            timesForEachLocationIndex[index]++;
+                        else
+                            timesForEachLocationIndex[index] = 1;
+                    }
+
+                    foreach (var entry in timesForEachLocationIndex)
+                    {
+                        if (!TryActionAtLocation(locations[entry.Key], settings, queryContext, triggerContext, entry.Value, out error))
+                            return false;
+                    }
+                    break;
+            }
+
+            error = null;
+            return true;
+        }
+
+        /********************/
+        /* Abstract methods */
+        /********************/
+
+        /// <summary>Tries to perform this action at the specified location.</summary>
+        /// <param name="location">The in-game location to use.</param>
+        /// <param name="settings">The custom action's settings.</param>
+        /// <param name="queryContext">Contextual information to use when checking conditions.</param>
+        /// <param name="triggerContext">Contextual information about a raised trigger.</param>
+        /// <param name="numberOfItems">The number of items to generate.</param>
+        /// <param name="error">Error text describing why items could not be spawned. Null if no errors occurred.</param>
+        /// <returns>True if spawning completed without errors, even if nothing was spawned (e.g. if tiles are blocked). False if any errors were encountered.</returns>
+        protected abstract bool TryActionAtLocation(GameLocation location, TSettings settings, GameStateQueryContext queryContext, TriggerActionContext triggerContext, int numberOfItems, out string error);
+    }
+}
--- FarmTypeManager/V2 Code/CustomActions/Handlers/Components/SpawnItemHandlerBase.cs
+++ FarmTypeManager/V2 Code/CustomActions/Handlers/Components/SpawnItemHandlerBase.cs
@@ -10,104 +10,20 @@ using System.Collections.Generic;
 namespace FarmTypeManager.CustomActions
 {
     /// <summary>A base for handlers that spawn a set of items at in-game locations.</summary>
-    public abstract class SpawnItemHandlerBase<TSettings> : ICustomActionHandler where TSettings : class, ILocationSettings, ITileSettings, ISpawnItemSettings, ITimesToPerformSettings
+    public abstract class SpawnItemHandlerBase<TSettings> : LocationHandlerBase<TSettings>, ICustomActionHandler where TSettings : class, ILocationSettings, ITileSettings, ISpawnItemSettings, ITimesToPerformSettings
     {
-        /************************/
-        /* ICustomActionHandler */
-        /************************/
+        /***********************/
+        /* LocationHandlerBase */
+        /***********************/
 
-        public string ProviderModId => FTMUtility.Manifest?.UniqueID;
-        public Type SettingsType => typeof(TSettings);
-        public bool TryPerform(string actionType, object rawSettings, GameStateQueryContext queryContext, TriggerActionContext triggerContext, out string error)
-        {
-            var settings = rawSettings as TSettings;
-            if (rawSettings == null)
-            {
-                error = "The provided settings are null.";
-                return false;
-            }
-            else if (settings == null)
-            {
-                error = $"The provided settings are an unsupported type: \"{rawSettings.GetType()?.ToString() ?? "null"}\".";
-                return false;
-            }
-
-            List<GameLocation> locations = settings.GetActiveLocations();
-            if (locations.Count <= 0)
-            {
-                error = null;
-                return true;
-            }
-
-            if (settings.MinTimes > settings.MaxTimes)
-            {
-                error = $"MinTimes ({settings.MinTimes}) is greater than MaxTimes ({settings.MaxTimes}).";
-                return false;
-            }
-
-            //get a random number from min to max, apply modifiers, and round to the nearest integer
-            int times = settings.GetRandomTimes(queryContext.Location, queryContext.Player, queryContext.TargetItem, queryContext.InputItem, queryContext.Random);
-            if (times <= 0)
-            {
-                error = null;
-                return true;
-            }
-
-            ItemQueryContext itemContext = new(queryContext.Location, queryContext.Player, FTMUtility.Random, $"FTM custom action handler. ActionType: \"{actionType}\". Trigger: \"{triggerContext.Trigger}\". Handler type: \"{typeof(SpawnObjectHandler)}\".");
-
-            switch (settings.LocationListMode)
-            {
-                case ILocationSettings.LocationListModes.All:
-                    foreach (GameLocation location in locations)
-                    {
-                        if (!TrySpawnItems(location, settings, queryContext, itemContext, times, out error))
-                            return false;
-                    }
-                    break;
-
-                case ILocationSettings.LocationListModes.Random:
-                default:
-                    int count = locations.Count;
-                    Dictionary<int, int> timesForEachLocationIndex = new(count); //key = random index in the location list; value = number of times to use that location
-                    for (int x = 0; x < times; x++)
-                    {
-                        int index = FTMUtility.Random.Next(count);
-                        if (timesForEachLocationIndex.ContainsKey(index))
-                            timesForEachLocationIndex[index]++;
-                        else
-                            timesForEachLocationIndex[index] = 1;
-                    }
-
-                    foreach (var entry in timesForEachLocationIndex)
-                    {
-                        if (!TrySpawnItems(locations[entry.Key], settings, queryContext, itemContext, entry.Value, out error))
-                            return false;
-                    }
-                    break;
-            }
-
-            error = null;
-            return true;
-        }
-
-        /****************/
-        /* Base methods */
-        /****************/
-
-        /// <summary>Spawns a set number of items at a location.</summary>
-        /// <param name="location">The in-game location to use.</param>
-        /// <param name="settings">The spawn settings to use.</param>
-        /// <param name="queryContext">Contextual information to use when checking conditions.</param>
-        /// <param name="itemContext">The item context to use when generating items.</param>
-        /// <param name="numberOfItems">The number of items to generate.</param>
-        /// <param name="error">Error text describing why items could not be spawned. Null if no errors occurred.</param>
-        /// <returns>True if spawning completed without errors, even if nothing was spawned (e.g. if tiles are blocked). False if any errors were encountered.</returns>
-        protected virtual bool TrySpawnItems(GameLocation location, TSettings settings, GameStateQueryContext queryContext, ItemQueryContext itemContext, int numberOfItems, out string error)
+        protected override bool TryActionAtLocation(GameLocation location, TSettings settings, GameStateQueryContext queryContext, TriggerActionContext triggerContext, int numberOfItems, out string error)
         {
             queryContext = new(location, queryContext.Player, queryContext.TargetItem, queryContext.InputItem, queryContext.Random, queryContext.IgnoreQueryKeys, queryContext.CustomFields); //use the current location for context
 
             var tileQuery = new TileCondition(location, settings.TileCondition);
             var tiles = tileQuery.GetTiles(true).GetEnumerator();
+
+            ItemQueryContext itemContext = new(location, queryContext.Player, queryContext.Random, $"FTM custom action handler. Trigger: \"{triggerContext.Trigger}\". Handler type: \"{GetType()}\".");
 
             int totalSpawned = 0;
             foreach (Item item in settings.CreateItems(queryContext, itemContext, numberOfItems, false))
@@ -134,6 +50,10 @@ namespace FarmTypeManager.CustomActions
             error = null;
             return true;
         }
+
+        /********************/
+        /* Abstract methods */
+        /********************/
 
         /// <summary>Places an item on a specified tile, if possible.</summary>
         /// <typeparam name="TItem">The type of <see cref="Item"/> being placed.</typeparam>
