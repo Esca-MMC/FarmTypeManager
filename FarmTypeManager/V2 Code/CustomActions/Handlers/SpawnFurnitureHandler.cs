c767a1b36ad02210f5aeb4fcaeb6b4f3a5078ccb
Esca-MMC <esca.mmc@gmail.com>
2025-09-28 18:29:27 -0500

    Add "spawn furniture" action

---
+++ FarmTypeManager/V2 Code/CustomActions/Handlers/SpawnFurnitureHandler.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+using StardewValley;
+using StardewValley.Extensions;
+using StardewValley.Objects;
+using Object = StardewValley.Object;
+
+namespace FarmTypeManager.CustomActions
+{
+    /// <summary>The handler for a custom action that spawns forage, craftables, or other basic placed <see cref="Object"/>s.</summary>
+    public class SpawnFurnitureHandler : SpawnItemHandlerBase<SpawnItemSettings>
+    {
+        protected override bool TryPlaceItem(GameLocation location, Vector2 tile, Item item, out string placementError)
+        {
+            if (item is Furniture furniture)
+            {
+                furniture.Location = location;
+                furniture.TileLocation = tile;
+                location.furniture.Add(furniture);
+
+                placementError = null;
+                return true;
+            }
+            else
+            {
+                placementError = $"This action can only place furniture (F).";
+                return false;
+            }
+        }
+    }
+}
